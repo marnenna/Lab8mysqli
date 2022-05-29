924942748eb0d48233d122da21f58f9d4c894982
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-29 15:24:12 +0800

    con_db

--- con_db.php
+++ con_db.php
@@ -14,15 +14,15 @@
     define('db_host','localhost');
     define('db_name','registration');
 
-    $result = mysqli_query($conn,"select * from user");
     $conn = mysqli_connect(db_host, db_user, db_password, db_name);
+    $result = mysqli_query($conn,"select * from user");
     while($row = mysqli_fetch_array($result)){
         foreach($row as $key=>$value){
             echo "$key = $value <br>\n";
         }
     }
-    mysqli_close($conn);
     mysqli_free_result($result);
+    mysqli_close($conn);
     
     ?>
 </body>
