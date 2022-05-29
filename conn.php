436ba9c9f015cbe31d47a900c7bb47c5b415625f
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-29 19:20:32 +0800

    conn

--- conn.php
+++ conn.php
@@ -17,7 +17,7 @@
 
     if (mysqli_connect_errno()){
 
-        echo "Failed to connect to MySQL: " . mysqli_connect_errno(); 
+        echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
     }
 
     ?>
