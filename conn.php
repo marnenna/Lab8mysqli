bf1ef278eefab214b766ae35bdae329c9bda0491
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-29 16:18:21 +0800

    conn

--- conn.php
+++ conn.php
@@ -13,10 +13,10 @@
     define('db_host','localhost');
     define('db_name','registration');
 
-    $conn = mysqli_connect (db_host, db_user, db_password, db_name);
+    $conn = mysqli_connect(db_host, db_user, db_password, db_name);
 
-    if(mysqli_connect_error()){
-        echo "Failed to connect to MySQL:" . mysqli_connect_error(); 
+    if (mysqli_connect_error()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
     }
 
     ?>
