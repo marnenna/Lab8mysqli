cd992b2bc5ddd6f3e326905ca907d90c707d8504
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-29 20:53:41 +0800

    add_user

--- add_user.php
+++ add_user.php
@@ -11,8 +11,8 @@
     
     include 'conn.php';
 
-    $sql= INSERT INTO user ('first_name', 'last_name', 'email', 'pass_word')
-    VALUES('".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['email']."', '".$_POST['password']."', '".$_POST['reg_date']."');
+    $sql= INSERT INTO user(first_name, last_name, email, pass_word, registration_date)
+    VALUES($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['password'], $_POST['reg_date']);
 
             if(!mysqli_query($conn, $sql)){
 
