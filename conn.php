9b893a0fcc2a40726a1f393d989f8cb63270239c
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-29 19:19:02 +0800

    conn

--- conn.php
+++ conn.php
@@ -15,9 +15,9 @@
 
     $conn = mysqli_connect (db_host, db_user, db_password, db_name);
 
-    if (mysqli_connect_error()){
+    if (mysqli_connect_errno()){
 
-        echo "Failed to connect to MySQL: " . $conn; 
+        echo "Failed to connect to MySQL: " . mysqli_connect_errno(); 
     }
 
     ?>
