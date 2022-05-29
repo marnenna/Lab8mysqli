9135dcdec3a6e32dc4d1e2ea3a19faf5462a96c2
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-05-29 19:38:24 +0800

    conn

--- conn.php
+++ conn.php
@@ -14,7 +14,7 @@
     $db_host = "localhost";
     $db_name = "registration";
 
-    $conn = mysqli_connect ($db_host, $db_user, $db_password, $db_name);
+    $conn = mysqli_connect (db_host, db_user, db_password, db_name);
 
     if (mysqli_connect_errno()){
 
